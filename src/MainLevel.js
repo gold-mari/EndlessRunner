223a8a1a8cc04ac11b75aa1ecc20103317525748
Dylan Mahler <dmahler@ucsc.edu>
2024-02-11 17:39:28 -0800

    Began implementing music + rotation

--- src/MainLevel.js
+++ src/MainLevel.js
@@ -4,7 +4,9 @@ class MainLevel extends Phaser.Scene {
     }
 
     init() {
-        console.log("MainLevel: init");   
+        console.log("MainLevel: init");
+        
+        this.ROTATION_VELOCITY = 2;
     }
 
     preload() {
@@ -12,15 +14,31 @@ class MainLevel extends Phaser.Scene {
     }
 
     create() {
+        // SPRITES AND ANIMATIONS
+        this.octagonA = this.add.sprite(width/2, height/2, "octagon").setScale(0.667);
+
+        // MUSIC AND SOUND
         this.music_intro = this.sound.add("music-intro");
         this.music_loop = this.sound.add("music-loop");
         this.music_loop.loop = true;
 
-        this.music_intro.on("complete", () => this.music_loop.play());
+        this.music_intro.once("complete", () => this.music_loop.play());
         this.music_intro.play();
+
+        // INPUT
+        cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
-        
+        let rotationAmount = 0
+        // handle left-right
+        if (cursors.left.isDown) rotationAmount += -1;
+        if (cursors.right.isDown) rotationAmount += 1;
+
+        let delta = rotationAmount * this.ROTATION_VELOCITY;
+        this.octagonA.angle += delta;
+        this.octagonB.angle += delta;
+        this.octagonC.angle += delta;
+        this.octagonD.angle += delta;
     }
 }
--- src/MainMenu.js
+++ src/MainMenu.js
@@ -5,15 +5,13 @@ class MainMenu extends Phaser.Scene
     }
 
     preload() {
+        this.load.image("octagon", "./assets/sprites/placeholder/octagon.png");
+
         this.load.audio("music-intro", "./assets/sound/placeholder/dive-drive_intro.mp3");
         this.load.audio("music-loop", "./assets/sound/placeholder/dive-drive_loop.mp3");
     }
 
     create() {
         this.scene.start("mainLevelScene");
-    }
-
-    update() {
-        
     }
 }
